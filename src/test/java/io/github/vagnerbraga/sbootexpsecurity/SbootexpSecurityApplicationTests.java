6e7ac1529c9bf8400cb5f256e5d12c4b32005684
Vagner <haigorne@gmail.com>
2025-08-26 12:23:13 -0300

    [sec-01] Criando as rotas e adicionando o Security no projeto

---
+++ src/test/java/io/github/vagnerbraga/sbootexpsecurity/SbootexpSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.vagnerbraga.sbootexpsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbootexpSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
