b7cfe46fcc71926b77c426aa941054d9c35d2c28
Vagner <haigorne@gmail.com>
2025-08-26 12:49:50 -0300

    [sec-02] Configuração base do Spring Security

---
+++ src/main/java/io/github/vagnerbraga/sbootexpsecurity/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package io.github.vagnerbraga.sbootexpsecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(customizer -> {
+                            customizer.requestMatchers("/public").permitAll();
+                            customizer.anyRequest().authenticated();
+                        })
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults())
+                .build();
+    }
+}
