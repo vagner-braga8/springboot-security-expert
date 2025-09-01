e2d536e9d3d7159103f7870fa2a2440fa0f00aa1
Vagner <haigorne@gmail.com>
2025-09-01 09:04:16 -0300

    [sec-05] Utilizando um Authentication Provider

---
+++ src/main/java/io/github/vagnerbraga/sbootexpsecurity/config/SenhaMasterAuthenticationProvider.java
@@ -0,0 +1,36 @@
+package io.github.vagnerbraga.sbootexpsecurity.config;
+
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class SenhaMasterAuthenticationProvider implements AuthenticationProvider {
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+
+        var login = authentication.getName();
+        var senha = authentication.getCredentials().toString();
+
+        String loginMaster = "master";
+        String senhaMaster = "@321";
+
+        if(loginMaster.equals(login) && senhaMaster.equals(senha)){
+            return new UsernamePasswordAuthenticationToken
+                    ("Sou master ", null, List.of(new SimpleGrantedAuthority("ADMIN")));
+        }
+
+        return null;
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return true;
+    }
+}
