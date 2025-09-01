50c3881c5bc765487f78c1e78f41a1cb791b96f6
Vagner <haigorne@gmail.com>
2025-09-01 08:47:59 -0300

    [sec-04] Entendendo o Authentication

--- src/main/java/io/github/vagnerbraga/sbootexpsecurity/api/FooController.java
+++ src/main/java/io/github/vagnerbraga/sbootexpsecurity/api/FooController.java
@@ -1,6 +1,7 @@
 package io.github.vagnerbraga.sbootexpsecurity.api;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +14,7 @@ public class FooController {
     }
 
     @GetMapping("/private")
-    public ResponseEntity<String> privateRoute(){
-        return ResponseEntity.ok("Private route ok!");
+    public ResponseEntity<String> privateRoute(Authentication authentication){
+        return ResponseEntity.ok("Private route ok! Usuário conectado: " + authentication.getName());
     }
 }
